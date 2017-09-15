2903b3ffe98418557166e96ff84a0bc1f968599e
Frederick Akalin <akalin@gmail.com>
2017-09-15 01:20:46 -0700

    Use preact and start a flow-type definition for it

--- carryless_demo.js
+++ carryless_demo.js
@@ -1,9 +1,11 @@
 // @flow
 
+/* eslint-env browser */
+
 'use strict';
 
 /* :: import { carrylessAdd } from './carryless'; */
-/* global carrylessAdd, HTMLInputElement */
+/* global preact, carrylessAdd */
 
 const padStart = (
   s /* : string */,
@@ -22,68 +24,129 @@ const padStart = (
   return padding.slice(0, padLength) + s;
 };
 
-// eslint-disable-next-line no-unused-vars
-const startCarrylessAddDemo = (root /* : Element */) => {
-  const [aInput, bInput] = root.querySelectorAll('input');
+/* ::
+type AddDetailProps = {
+  a: number,
+  b: number,
+  sum: number,
+};
+*/
 
-  if (
-    !(aInput instanceof HTMLInputElement) ||
-    !(bInput instanceof HTMLInputElement)
-  ) {
-    throw new Error('aInput or bInput unexpectedly not an HTMLInputElement');
-  }
+const AddDetail = ({ a, b, sum } /* : AddDetailProps */) => {
+  const aDec = a.toString(10);
+  const bDec = b.toString(10);
 
-  const calcNode = root.querySelector('pre');
-  if (calcNode === null) {
-    throw new Error('calcNode unexpectedly null');
-  }
+  const aBin = a.toString(2);
+  const bBin = b.toString(2);
+  const sumBin = sum.toString(2);
 
-  const calcOutput = calcNode.childNodes[0];
-  const [binaryOutputNode, decimalOutputNode] = root.querySelectorAll(
-    'span > span'
-  );
-  const [binaryOutput, decimalOutput] = [
-    binaryOutputNode.childNodes[0],
-    decimalOutputNode.childNodes[0],
-  ];
+  const decPadLength = Math.max(aDec.length, bDec.length);
 
-  const onInput = () => {
-    // TODO: Sanity-check a and b.
-    const a = parseInt(aInput.value, 10);
-    const b = parseInt(bInput.value, 10);
-    const sum = carrylessAdd(a, b);
+  const aDecPadded = padStart(aDec, decPadLength, ' ');
+  const bDecPadded = padStart(bDec, decPadLength, ' ');
+  const decSpacer = ' '.repeat(decPadLength);
 
-    const aDec = a.toString(10);
-    const bDec = b.toString(10);
-    const sumDec = sum.toString(10);
+  const binPadLength = Math.max(aBin.length, bBin.length, sumBin.length);
 
-    const aBin = a.toString(2);
-    const bBin = b.toString(2);
-    const sumBin = sum.toString(2);
+  const aBinPadded = padStart(aBin, binPadLength, ' ');
+  const bBinPadded = padStart(bBin, binPadLength, ' ');
+  const sumBinPadded = padStart(sumBin, binPadLength, ' ');
+  const binDivider = '-'.repeat(binPadLength);
 
-    const decPadLength = Math.max(aDec.length, bDec.length);
-
-    const aDecPadded = padStart(aDec, decPadLength, ' ');
-    const bDecPadded = padStart(bDec, decPadLength, ' ');
-    const decSpacer = ' '.repeat(decPadLength);
-
-    const binPadLength = Math.max(aBin.length, bBin.length, sumBin.length);
-
-    const aBinPadded = padStart(aBin, binPadLength, ' ');
-    const bBinPadded = padStart(bBin, binPadLength, ' ');
-    const sumBinPadded = padStart(sumBin, binPadLength, ' ');
-    const binDivider = '-'.repeat(binPadLength);
-
-    calcOutput.nodeValue = `  a = ${aDecPadded} = ${aBinPadded}b
+  const output = `  a = ${aDecPadded} = ${aBinPadded}b
 ^ b = ${bDecPadded} = ${bBinPadded}b
       ${decSpacer}   ${binDivider}-
       ${decSpacer}   ${sumBinPadded}b`;
-    binaryOutput.nodeValue = sumBin;
-    decimalOutput.nodeValue = sumDec;
-  };
 
-  aInput.addEventListener('input', onInput);
-  bInput.addEventListener('input', onInput);
+  return preact.h('pre', null, output);
 };
 
-/* :: export { startCarrylessAddDemo }; */
+const getTargetInput = (e /* : Event */) /* : HTMLInputElement */ => {
+  if (!(e.target instanceof HTMLInputElement)) {
+    throw new Error('e.target unexpectedly not an HTMLInputElement');
+  }
+  return e.target;
+};
+
+const spanNoWrap = (...children) =>
+  preact.h('span', { style: { whiteSpace: 'nowrap' } }, ...children);
+
+/* ::
+type AddDemoState = {
+  a: number,
+  b: number,
+};
+*/
+
+// eslint-disable-next-line no-unused-vars
+class AddDemo extends preact.Component /* :: <{}, AddDemoState> */ {
+  constructor(
+    { initialA: a, initialB: b } /* : { initialA: number, initialB: number } */
+  ) {
+    super({});
+    // Workaround for https://github.com/prettier/prettier/issues/719 .
+    const state /* : AddDemoState */ = { a, b };
+    this.state = state;
+  }
+
+  onAChange(a /* : number */) {
+    // Should be { ...state, a }, but that is unsupported by Safari.
+    this.setState(state => ({ a, b: state.b }));
+  }
+
+  onBChange(b /* : number */) {
+    // Should be { ...state, b }, but that is unsupported by Safari.
+    this.setState(state => ({ a: state.a, b }));
+  }
+
+  render(props /* : {} */, { a, b } /* : AddDemoState */) {
+    const { h } = preact;
+
+    // TODO: Sanity-check a and b.
+    const sum = carrylessAdd(a, b);
+    const aVar = h('var', null, 'a');
+    const bVar = h('var', null, 'b');
+    return h(
+      'div',
+      null,
+      'Let ',
+      spanNoWrap(
+        aVar,
+        ' = ',
+        h('input', {
+          type: 'text',
+          value: a.toString(10),
+          onInput: (e /* : Event */) => {
+            const input = getTargetInput(e);
+            return this.onAChange(parseInt(input.value, 10));
+          },
+        })
+      ),
+      ' and ',
+      spanNoWrap(
+        bVar,
+        ' = ',
+        h('input', {
+          type: 'text',
+          value: b.toString(10),
+          onInput: (e /* : Event */) => {
+            const input = getTargetInput(e);
+            return this.onBChange(parseInt(input.value, 10));
+          },
+        }),
+        '.'
+      ),
+      ' Then ',
+      h(AddDetail, { a, b, sum }),
+      ' so ',
+      spanNoWrap(
+        aVar,
+        ' ⊕ ',
+        bVar,
+        ` = ${sum.toString(2)}`,
+        h('sub', null, 'b'),
+        ` = ${sum.toString(10)}.`
+      )
+    );
+  }
+}
---
+++ flow-typed/preact.js
@@ -0,0 +1,17 @@
+declare class Component<Props, State> {
+  props: Props;
+  state: State;
+
+  constructor(props: Props): Component<Props, State>;
+
+  setState(state: State | (state: State) => State): void;
+};
+
+declare class VNode {};
+
+type VChild = void | null | boolean | number | string | VNode | HTMLElement | VChild[];
+
+declare var preact: {
+  h(nodeName: string | Function, attributes: ?{}, ...children: VChild[]): VNode;
+  Component: typeof Component,
+};
