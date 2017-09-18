35bd59f8875fac7391838b0672b9119e21fdabd8
Frederick Akalin <akalin@gmail.com>
2017-09-18 01:41:27 -0700

    Add Matrix class and tests for it

---
+++ matrix.js
@@ -0,0 +1,78 @@
+// @flow
+
+'use strict';
+
+/* :: import { type Field } from './field'; */
+
+// eslint-disable-next-line no-unused-vars
+class Matrix /* :: <T: Field<*>> */ {
+  /* ::
+  _rows: number;
+  _columns: number;
+  _elements: T[];
+  */
+
+  constructor(
+    rows /* : number */,
+    columns /* : number */,
+    elements /* : T[] | (number, number) => T */
+  ) {
+    if (typeof elements !== 'function' && elements.length !== rows * columns) {
+      throw new Error('Element/row/column count mismatch');
+    }
+    this._rows = rows;
+    this._columns = columns;
+    if (typeof elements === 'function') {
+      this._elements = [];
+      for (let i = 0; i < rows; i += 1) {
+        for (let j = 0; j < columns; j += 1) {
+          this._elements.push(elements(i, j));
+        }
+      }
+    } else {
+      this._elements = elements.slice();
+    }
+    if (this._elements.length === 0) {
+      throw new Error('Empty matrix');
+    }
+  }
+
+  zeroElement() /* : T */ {
+    return this._elements[0].zero();
+  }
+
+  oneElement() /* : T */ {
+    return this._elements[0].one();
+  }
+
+  at(i /* : number */, j /* : number */) /* : T */ {
+    if (i < 0 || i >= this._rows) {
+      throw new RangeError('Row index out of bounds');
+    }
+    if (j < 0 || j >= this._columns) {
+      throw new RangeError('Column index out of bounds');
+    }
+    return this._elements[i * this._columns + j];
+  }
+
+  times(n /* : Matrix<T> */) /* : Matrix<T> */ {
+    if (this._columns !== n._rows) {
+      throw new Error('Mismatched dimensions');
+    }
+    const columns = this._columns;
+    return new Matrix(this._rows, n._columns, (
+      i /* : number */,
+      j /* : number */
+    ) /* : T */ => {
+      let t = this.zeroElement();
+      for (let k = 0; k < columns; k += 1) {
+        t = t.plus(this.at(i, k).times(n.at(k, j)));
+      }
+      return t;
+    });
+  }
+}
+
+/* ::
+export { Matrix };
+*/
