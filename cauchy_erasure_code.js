24dd15d6d4778ff8d543c7d5e89f68917bcf487e
Frederick Akalin <akalin@gmail.com>
2017-11-21 17:03:50 -0600

    Implement reconstructData and add tests

--- cauchy_erasure_code.js
+++ cauchy_erasure_code.js
@@ -36,6 +36,63 @@ const computeParity = (
   return pCol.elements();
 };
 
+class NotEnoughKnownBytesError extends Error {
+  // flowlint-next-line unclear-type:off
+  constructor(...params /* : any[] */) {
+    super('Not enough known bytes to reconstruct', ...params);
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, NotEnoughKnownBytesError);
+    }
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
+const reconstructData = (
+  partialD /* : (?Field256Element)[] */,
+  partialP /* : (?Field256Element)[] */
+) /* : Field256Element[] */ => {
+  const n = partialD.length;
+  const m = partialP.length;
+
+  const knownData = [];
+  const knownDataIndices = [];
+  for (let i = 0; i < n; i += 1) {
+    if (partialD[i] instanceof Field256Element) {
+      knownData.push(partialD[i]);
+      knownDataIndices.push(i);
+    }
+  }
+
+  const parityToUse = [];
+  const parityIndicesToUse = [];
+  for (let i = 0; i < m && knownData.length + parityToUse.length < n; i += 1) {
+    if (partialP[i] instanceof Field256Element) {
+      parityToUse.push(partialP[i]);
+      parityIndicesToUse.push(i);
+    }
+  }
+
+  if (knownData.length + parityToUse.length < n) {
+    throw new NotEnoughKnownBytesError();
+  }
+
+  const P = computeParityMatrix(n, m);
+  const M = new Matrix(n, n, (i, j) => {
+    if (i < knownDataIndices.length) {
+      return knownDataIndices[i] === j
+        ? Field256Element.One
+        : Field256Element.Zero;
+    }
+    return P.at(parityIndicesToUse[i - knownDataIndices.length], j);
+  });
+  const MInv = M.inverse();
+
+  const bytesToUse = knownData.concat(parityToUse);
+  const bytesToUseCol = new Matrix(n, 1, bytesToUse);
+  const dCol = MInv.times(bytesToUseCol);
+  return dCol.elements();
+};
+
 /* ::
-export { computeParityMatrix, computeParity };
+export { computeParityMatrix, computeParity, reconstructData };
 */
--- cauchy_erasure_code_tests.js
+++ cauchy_erasure_code_tests.js
@@ -7,9 +7,22 @@
 /* ::
 import { Field256Element } from './field_256';
 import { Matrix } from './matrix';
-import { computeParityMatrix, computeParity } from './cauchy_erasure_code';
+import {
+  computeParityMatrix,
+  computeParity,
+  reconstructData,
+} from './cauchy_erasure_code';
 */
-/* global Field256Element, Matrix, computeParityMatrix, computeParity */
+/*
+global
+ Field256Element,
+
+  Matrix,
+
+  computeParityMatrix,
+  computeParity,
+  reconstructData,
+*/
 
 describe('Cauchy erasure code', () => {
   it('computeParityMatrix', () => {
@@ -35,9 +48,55 @@ describe('Cauchy erasure code', () => {
     );
   });
 
+  const to256 = a => a.map(x => new Field256Element(x));
+
   it('computeParity', () => {
     const d = [0xda, 0xdb, 0x0d];
-    const p = computeParity(d.map(x => new Field256Element(x)), 2);
-    expect(p).toEqual([0x52, 0x0c].map(x => new Field256Element(x)));
+    const p = computeParity(to256(d), 2);
+    expect(p).toEqual(to256([0x52, 0x0c]));
+  });
+
+  const to256Partial = a =>
+    a.map(x => (typeof x === 'number' ? new Field256Element(x) : x));
+
+  it('reconstructData 0 missing', () => {
+    const partialD = [0xda, 0xdb, 0x0d];
+    const p = [null, null];
+    const d = reconstructData(to256Partial(partialD), to256Partial(p));
+    expect(d).toEqual(to256([0xda, 0xdb, 0x0d]));
+  });
+
+  it('reconstructData 1 missing', () => {
+    const partialD = [0xda, null, 0x0d];
+    const p = [null, 0x0c];
+    const d = reconstructData(to256Partial(partialD), to256Partial(p));
+    expect(d).toEqual(to256([0xda, 0xdb, 0x0d]));
+  });
+
+  it('reconstructData 2 missing', () => {
+    const partialD = [null, 0xdb, null];
+    const p = [0x52, 0x0c];
+    const d = reconstructData(to256Partial(partialD), to256Partial(p));
+    expect(d).toEqual(to256([0xda, 0xdb, 0x0d]));
+  });
+
+  it('reconstructData 3 missing', () => {
+    const partialD = [null, 0xdb, null];
+    const p = [0x52, null];
+    expect(() =>
+      reconstructData(to256Partial(partialD), to256Partial(p))
+    ).toThrowError('Not enough known bytes to reconstruct');
+  });
+
+  it('reconstructData error', () => {
+    expect(() => reconstructData([], [])).toThrowError('Empty matrix');
+    const partialD = [];
+    for (let i = 0; i < 256; i += 1) {
+      partialD.push(Field256Element.One);
+    }
+    const knownP = [Field256Element.One];
+    expect(() => reconstructData(partialD, knownP)).toThrowError(
+      'n must be >= 0 and < 256'
+    );
   });
 });
